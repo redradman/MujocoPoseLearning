472894d6b10fe952d0df8d7426791a057fafbac7
Radman <rdmnr@protonmail.com>
2024-12-09 19:48:43 -0800

    clean and update hyperparam

--- train_sb3.py
+++ train_sb3.py
@@ -164,24 +164,23 @@ def main():
     model = PPO(
         "MlpPolicy",
         env,
-        learning_rate=3e-5,
+        learning_rate=3e-4,
         n_steps=2048,
         batch_size=128,
         n_epochs=10,
         gamma=0.99,
         gae_lambda=0.95,
-        clip_range=0.3,
-        ent_coef=0.00234,
+        clip_range=0.2,
         max_grad_norm=2,
         tensorboard_log=str(storage_path / "tensorboard_logs"),
         verbose=1,
         policy_kwargs=dict(
-            log_std_init=-2,
+            log_std_init=-1,
             ortho_init=False,
             activation_fn=torch.nn.ReLU,
             net_arch=dict(
-                pi=[128, 128], 
-                vf=[128, 128]
+                pi=[64, 64], 
+                vf=[64, 64]
             )
         )
     )
