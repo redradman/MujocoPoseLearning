1d7dc84bcc8dec55d8f1cdac77fd2c3ed21dda33
Radman <rdmnr@protonmail.com>
2024-12-12 18:52:29 -0800

    add config file

---
+++ config.py
@@ -0,0 +1,34 @@
+"""
+Sample configuration file for humanoid training
+"""
+
+config = {
+    # Environment parameters
+    "env_kwargs": {
+        "total_timesteps": 5_000_000,
+        "render_interval": 2500,
+        "n_envs": 8,
+        "reward_function": "walk",
+        "frame_skip": 3,
+        "framerate": 60
+    },
+    
+    # PPO parameters
+    "ppo_kwargs": {
+        "learning_rate": 3e-4,
+        "n_steps": 2048,
+        "batch_size": 256,
+        "n_epochs": 20,
+        "gamma": 0.99,
+        "gae_lambda": 0.95,
+        "clip_range": 0.2,
+        "ent_coef": 0.0,
+        "policy_kwargs": {
+            "activation_fn": "ReLU",  # Will be converted to torch.nn.ReLU
+            "net_arch": {
+                "pi": [64, 64],
+                "vf": [64, 64]
+            }
+        }
+    }
+} 
